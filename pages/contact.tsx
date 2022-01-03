42ad50dd7953582518f2fdbd38c53e9b2b0f2451
narthkings <imohdumto@gmail.com>
2022-01-03 10:26:17 +0100

    contact us form layout implemented, formik installed

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,6 @@
 import type { ReactElement } from 'react'
-import type { NextPage } from 'next'
 
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, Text, Input, Textarea, FormControl, FormLabel, FormErrorMessage, FormHelperText, } from '@chakra-ui/react'
 import Layout from '../components/Layout'
 
 
@@ -9,8 +8,15 @@ const Contact = () => {
 
     return (
         <Box bg='black' height={"92vh"} color="white" >
-            <Flex justifyContent={'center'}>
+            <Flex direction={'column'} alignItems={'center'} justifyContent={'center'}>
                 <Text fontSize={'2xl'}>{'<'}Contact{'>'}</Text>
+                <Text fontStyle={'italic'} fontSize={'md'}>You wanna reach out to me 💌</Text>
+            </Flex>
+
+            <Flex direction={'column'} alignItems={'center'} justifyContent={'center'} marginTop={'4rem'}>
+                <Input variant='filled' placeholder='Enter Name' size='lg' width={'50%'} />
+                <Input variant='filled' placeholder='Enter Email address' size='lg' width={'50%'} marginTop={'2rem'} required/>
+                <Textarea variant='filled' placeholder="Amazing Portfolio, I’d like you to work with my team on a project ……….." width={'50%'} size="lg" height={'8rem'} marginTop={'2rem'}/>
             </Flex>
         </Box>
     )
