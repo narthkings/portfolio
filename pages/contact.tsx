d0e4b7864913f604b7c8c19712f75cd93957aa10
narthkings <imohdumto@gmail.com>
2022-01-06 08:37:42 +0100

    yup validtion with formik implemented

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,23 +1,65 @@
 import type { ReactElement } from 'react'
-
-import { Box, Flex, Text, Input, Textarea, FormControl, FormLabel, FormErrorMessage, FormHelperText, } from '@chakra-ui/react'
+import { Formik, Form } from 'formik';
+import { Box, Flex, Text, Input, Textarea, FormControl, Button, FormHelperText, } from '@chakra-ui/react'
 import Layout from '../components/Layout'
-
+import { MessageSchema } from '../utilities/schema';
+import { Message } from '../types';
 
 const Contact = () => {
+    const initialValues = {
+        name: '',
+        email: '',
+        message: '',
+    }
+
+    const onSubmit = (details: Message, { resetForm }: any) => {
+
+        resetForm()
+    }
+
 
     return (
-        <Box bg='black' height={"92vh"} color="white" >
+        <Box bg='black' height={"92vh"} color="white">
             <Flex direction={'column'} alignItems={'center'} justifyContent={'center'}>
                 <Text fontSize={'2xl'}>{'<'}Contact{'>'}</Text>
                 <Text fontStyle={'italic'} fontSize={'md'}>You wanna reach out to me 💌</Text>
             </Flex>
 
-            <Flex direction={'column'} alignItems={'center'} justifyContent={'center'} marginTop={'4rem'}>
-                <Input variant='filled' placeholder='Enter Name' size='lg' width={'50%'} />
-                <Input variant='filled' placeholder='Enter Email address' size='lg' width={'50%'} marginTop={'2rem'} required/>
-                <Textarea variant='filled' placeholder="Amazing Portfolio, I’d like you to work with my team on a project ……….." width={'50%'} size="lg" height={'8rem'} marginTop={'2rem'}/>
-            </Flex>
+            <Formik validationSchema={MessageSchema} initialValues={initialValues} onSubmit={onSubmit}>
+                {({ values: details, handleSubmit, handleChange, handleBlur, errors, touched }) => (
+                    <Form onSubmit={handleSubmit} >
+                        <Box textAlign={'center'} display={'flex'} flexDirection={'column'} justifyContent={'center'} marginTop={'5rem'}>
+                            <FormControl id='name'>
+                                <Input id='name' name="name" onChange={handleChange} onBlur={handleBlur} value={details.name} variant='filled' placeholder='Enter Name' size='lg' width={'50%'} />
+                                <FormHelperText color="red">{errors.name && touched.name && errors.name}</FormHelperText>
+                            </FormControl>
+
+                            <FormControl id='email'>
+                                <Input id="email" name='email' onChange={handleChange} value={details.email} onBlur={handleBlur} variant='filled' placeholder='Enter Email address' size='lg' width={'50%'} marginTop={'2rem'} />
+                                <FormHelperText color="red">{errors.email && touched.email && errors.email}</FormHelperText>
+                            </FormControl>
+
+                            <FormControl id="message">
+                                <Textarea id="message" name='message' onChange={handleChange} onBlur={handleBlur} value={details.message} variant='filled' placeholder="Amazing Portfolio, I’d like you to work with my team on a project ………" width={'50%'} size="lg" height={'8rem'} marginTop={'2rem'} />
+                                <FormHelperText color="red">{errors.message && touched.message && errors.message}</FormHelperText>
+                            </FormControl>
+                        </Box>
+                        <Box display={'flex'} justifyContent={'center'}>
+                            <Button
+                                mt={4}
+                                width={'15%'}
+                                color={'primary'}
+                                size={'md'}
+                                background={'secondary'}
+                                // isLoading={props.isSubmitting}
+                                type='submit'
+                            >
+                                Send Message
+                            </Button>
+                        </Box>
+                    </Form>
+                )}
+            </Formik>
         </Box>
     )
 }
--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ type GetLayoutFunc = (page: ReactElement) => ReactElement;
 export type NextPageWithLayout = NextPage & {
     getLayout?: GetLayoutFunc;
 }
+export type Message = {
+    name: string;
+    email: string;
+    message: string;
+}
