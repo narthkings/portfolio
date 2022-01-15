dd81a5fd70368a8caec139d6ce334ad8435af4fd
narthkings <imohdumto@gmail.com>
2022-01-15 21:15:35 +0100

    fix on header logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,10 +10,10 @@ const Header = ({ onOpen }: Iprops) => {
 
     return (
         <Flex zIndex={'11'} position={'fixed'} width={{ base: '100%', md: '91%', lg: '95%' }} bg="primary" padding={'2rem'} height="8%" justifyContent={'space-between'}>
-            <Box opacity={1} width="5%" position={'relative'} bg="gray.100" pl="2" height={'2.3rem'}>
+            <Box opacity={1} width={{base:'15%', md:"5%"}} position={'relative'} bg="gray.100" pl="2" height={{base:'2rem', lg:'2.3rem'}}>
                 <NextLink href='/' passHref>
                     <Link>
-                       <Text width="10rem" position={'absolute'} textTransform={'uppercase'} fontWeight={'bold'} fontSize={'2xl'} color="white">Dumto imoh</Text> 
+                       <Text width="10rem" position={'absolute'} textTransform={'uppercase'} fontWeight={'bold'} fontSize={{base:'lg', lg:'2xl'}} color="white">Dumto imoh</Text> 
                     </Link>
                 </NextLink>
             </Box>
